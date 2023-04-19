65e6ca02ec1d0eb26a40c3e7559b46f10b0dd2c5
Kamal Bisht <111355794+kml562@users.noreply.github.com>
2023-04-20 01:32:25 +0530

    done assigment

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 const express = require('express');
-var bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 
-const route = require('./routes/route.js');
+const router = require('./routes/route.js');
 
-const app = express();
+const app = express()
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/', route);
+app.use('/', router);
 
 app.listen(process.env.PORT || 3000, function() {
     console.log('Express app running on port ' + (process.env.PORT || 3000))
