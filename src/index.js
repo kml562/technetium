439d59062d09f2c103013b759b9a1bc077421025
Kamal Bisht <111355794+kml562@users.noreply.github.com>
2023-05-03 01:49:05 +0530

    do

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://kamal:bQ84rpZscLlgwVPN@cluster0.uj2soph.mongodb.net/populate-DB?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
