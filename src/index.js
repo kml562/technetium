0a5b9fb0ac29a73a43d2edb6647045ce80188f30
Kamal Bisht <111355794+kml562@users.noreply.github.com>
2023-05-12 20:50:24 +0530

    done

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://users-open-to-all:hiPassword123@cluster0.uh35t.mongodb.net/skDB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://kamal:bQ84rpZscLlgwVPN@cluster0.uj2soph.mongodb.net/jwt-DB", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
--- src/models/userModel.js
+++ src/models/userModel.js
@@ -10,6 +10,10 @@ const userSchema = new mongoose.Schema( {
     },
     emailId: String,
     password: String,
+    isDeleted:{
+type: Boolean,
+default: false,
+    },
     gender: {
         type: String,
         enum: ["male", "female", "other"]
@@ -19,3 +23,17 @@ const userSchema = new mongoose.Schema( {
 }, { timestamps: true });
 
 module.exports = mongoose.model('User', userSchema)
+    /* 	{
+    "_id" : ObjectId("6226e3d2b98f22b349ca58be"),
+    "firstName" : "Sabiha",
+    "lastName" : "Khan",
+    "mobile" : "9898909087",
+    "emailId" : "sk@gmail.com",
+    "password" : "password123",
+    "gender" : "female",
+	"isDeleted": false, //default value is false 
+    "age" : 12,
+    "createdAt" : ISODate("2022-03-08T05:04:18.737Z"),
+    "updatedAt" : ISODate("2022-03-08T05:04:18.737Z"),
+    "__v" : 0
+}   */
